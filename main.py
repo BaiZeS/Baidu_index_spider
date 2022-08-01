32d110404cd53e7473221e4828dd031df2d591b5
BaiZeS <43888701+BaiZeS@users.noreply.github.com>
2022-08-01 19:08:00 +0800

    fixed func of add res_list

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from mysql_server import *
 
 # 预定义默认参数
 log_path = os.path.join(os.path.dirname(__file__), 'error_log.log')
-Cipher_Text = '1658214009400_1658223740218_OLfvqUFw/ZzscbkL/KDpmvjFgFkFdgCro5si+uhDOxjs5+WSszjIhs+9NwFc4heAodSCUdemQ5QAtx+cb0f8MNJHAUH42D31enVRDo4MWI4CGjBfHgyxzsnSY42rHl5noFc61HQZf1/Clj6dkxLsuchviWZxbogVWoXHXIM6z4K++o30VdXw0CfjuP9IDKYlAdoh/rqX2F/5FM3wBz6RNG3EHu4CZU436lII0pOsqDebcy/nEREQ3+g58I7Mbe0Nr5nEx8XubP43OHPHWhrvbWMjKwySJJLyo4d75Jtm/Kw4o2hioTf3H7Sd5t0UvAm+FhbanxL6BD/K9rBHZRJKCWYnGrKmbFx3DALeVy/Al7o03ok0/eyux6jO1EWb98RQL+DU3j10CxtWLM9Habun4+/fEM8lpD8Vel+D9xS+LK1EODpeRMKH6gecAGYPwVXKTSq39ujQW9kOP/8MXKYz8fXJaLoasNLrutih3lE9e5d8BiIvvZz1chrzr5sB92DG'
-cookies = '__yjs_duid=1_0fe663a1123945dfda4fe82aa5e55fe91632706119900; BAIDUID=D9C58B588D6A9AB5743335E01333588A:FG=1; BIDUPSID=D9C58B588D6A9AB5743335E01333588A; PSTM=1634812464; Hm_lvt_d101ea4d2a5c67dab98251f0b5de24dc=1635935198; MCITY=-289:; BAIDUID_BFESS=0194F9EC3BAB2BDE0E3BA8F6830274B0:FG=1; ZFY=h3sfUf:ALkhqnY42AfoSwyMqfWFhxvAS4L:AYE65RFFBo:C; BDUSS=FPTTV4ZXVMVVMySkpDYUIzV3hsUnJZNFBwVnB4dWp4REhFRG1yQ0VVelVsdnhpSVFBQUFBJCQAAAAAAAAAAAEAAAB9DdNQtc~LucLtya3S1Mj2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANQJ1WLUCdViNG; bdindexid=bnfoldk0kar4b370m6tqpmie51; ab_sr=1.0.1_NmI2MDMzZDE2YzY2ZWE4YWZmMDUzNDgyNDMwOGQ0N2U0MTgzMGFmNGVlNDY0YzFhMTgwNTQ3N2RlYjE5OTdlNTUxMjVhMzllZGIzM2E3NTQxZTE3MmE2ZGM0Yzg1ZGM4MTMwYjkwZjc5MTA3MzljZDY3ZWFlOWQzYjU0ZGE2OGUxNTU3Y2VjMDBiOWU4NmY1MWYwZWQ0YzQ0NDQyYTgxMg==; RT="z=1&dm=baidu.com&si=20fdbae5-c2ea-4c69-b747-58dbd99b7d03&ss=l5rzkhhc&sl=5&tt=9zm&bcn=https://fclog.baidu.com/log/weirwood?type=perf"; BDUSS_BFESS=FPTTV4ZXVMVVMySkpDYUIzV3hsUnJZNFBwVnB4dWp4REhFRG1yQ0VVelVsdnhpSVFBQUFBJCQAAAAAAAAAAAEAAAB9DdNQtc~LucLtya3S1Mj2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANQJ1WLUCdViNG'
+Cipher_Text = '1659337210605_1659351285981_N3qnOnIhz1xe8id6yPQYjjZCehK6bVBVcZ0W+hkx8RbQwdd/5XXPuNv9xPAa1l9TNiq+n0rCgXl7Gr7AZtB9OfyPcUmKFrIw5bF9fZTOQsMxRf/9iCJEC1iY8XOV3alz7vO6PecHqBpmwDhMJlVUwKrJil0Kxg0SuIax6hObS62LX+f47hpcPnEv3rnnB2aU2v/uzn+ty3F0D9bvGJ+E741qR2SWVJpEE+6gGHf9XvjznaWCbl+Z7gA24aKe/79vqMm2SKN50J2cLmJzfhc9ZozZ64gNUoTW/OVZVlJ2ig92RvRAJkEBLc5weL9gSwT3B4nVz2NQrIuwcn6YtKZ8io6jbko2RRoTRn7IiyefND2hoBpr+zJ7xtJbYwvEd3lgInQaDovdbXJp9y1yNOHLH1EW9+gW/EokSV1x2a2Mmyc='
+cookies = '__yjs_duid=1_0fe663a1123945dfda4fe82aa5e55fe91632706119900; BAIDUID=D9C58B588D6A9AB5743335E01333588A:FG=1; BIDUPSID=D9C58B588D6A9AB5743335E01333588A; PSTM=1634812464; Hm_lvt_d101ea4d2a5c67dab98251f0b5de24dc=1635935198; MCITY=-289:; BAIDUID_BFESS=0194F9EC3BAB2BDE0E3BA8F6830274B0:FG=1; ZFY=h3sfUf:ALkhqnY42AfoSwyMqfWFhxvAS4L:AYE65RFFBo:C; BDUSS=01VLTRKRVNLTEtac3NQVk0zfmp3TU4xWXhvLWp3LXZ3a1RWZ35GLVJaTFVQUTlqSVFBQUFBJCQAAAAAAAAAAAEAAAB9DdNQtc~LucLtya3S1Mj2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANSw52LUsOdib; SIGNIN_UC=70a2711cf1d3d9b1a82d2f87d633bd8a04093637011C3qIs3L2LUlNx86sWcVoAZaYtC8JnM05AMJYOeKBGEbKK2t/hHOWW3AuPeMwOo8QxXjSX/JIgpR9neBIOtTvSYtEcK8rx9QikL1NrKVbMZmZWsjXK8vhQlmgTlfgKzApqVDRorqw1UkAxoPd6KwT+wfcdmlcC08NqVod9IlHcTsILnt+0TAB9yk0zINm6H/eqVDRorqw1UkAxoPd6KwT+wfcdmlcC08NqVod9IlHcTs7AISwlcnM2YSfeYTwMGC9N9WFc3NI3S6Frl6M4OkQLMs+SQSXDLqjUAW0sxFYS8dyXrqo4RTmOz0Zt3WJ4ZBPAUB0AX1V4BwddYCaScbraCUtyMyamGXQWxwlY/dffKU=32438252368981359275114084110211; __cas__rn__=409363701; __cas__st__212=467e7f74bfeb54795c599626434883b79b77d732359fc22b264d7913546801e1ebf06e3195722c1cbaa59762; __cas__id__212=41049790; CPID_212=41049790; CPTK_212=2073575016; bdindexid=6th9k0vnctf7oknhffo4cu7um5; ab_sr=1.0.1_MDQwYTIwZjg4N2RhYzYxZTZiNmQ3MjUyMzE2ZGI3N2U0MDAzNjgyYzUwNmZhZjI1OTVjOWIwMDA4OWMxNTYwODZhZjhjNWQ3OWFiMjUxMDIxYmIzZThmMDQ3YTRjMDBiNjQ0YjEwMDNhMTliMjFjYjBjY2YwNGI4NmVkMDA2OGQzOWI0NGJkZWIzZWVlZWMwYTBlZDYwOGYyMDNiYjAwMQ==; RT="z=1&dm=baidu.com&si=5f5e5b4a-1ee7-4d65-b884-a333a9d0036f&ss=l6amvjli&sl=a&tt=dgc&bcn=https://fclog.baidu.com/log/weirwood?type=perf"; BDUSS_BFESS=01VLTRKRVNLTEtac3NQVk0zfmp3TU4xWXhvLWp3LXZ3a1RWZ35GLVJaTFVQUTlqSVFBQUFBJCQAAAAAAAAAAAEAAAB9DdNQtc~LucLtya3S1Mj2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANSw52LUsOdib'
 
 
 def define_header(cookies, Ciper_Text):
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         # 获取当前小时
         time_now = datetime.datetime.now()
         # 定时每天8点运行
-        if time_now.hour == 17:
+        if time_now.hour == 19:
             # 从数据库中获取搜索关键字
             data_list = get_keywords()
             # 标记写入数据库位置
@@ -57,8 +57,8 @@ if __name__ == "__main__":
                         search_index = get_search_index(headers, keywords, days)
 
                         # 拼接更新数据表
-                        index_list.append(search_index, feed_index, sex_rate['男'], sex_rate['女'], age_rate['0-19'], age_rate['20-29'], age_rate['30-39'], age_rate['40-49'], age_rate['50+'], keywords)
-                        print('title %s, and the count of title: %s' % (keywords, len(index_list)))
+                        index_list.extend((search_index, feed_index, sex_rate['男'], sex_rate['女'], age_rate['0-19'], age_rate['20-29'], age_rate['30-39'], age_rate['40-49'], age_rate['50+'], keywords))
+                        print('title %s, and the count of title: %s' % (keywords, index_list))
 
                     except Exception as e:
                         # 记录错误日志
